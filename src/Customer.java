84293934783925e8bea3aefed725393efefb35c1
Paulo Ricardo <pauloricardosn10@gmail.com>
2024-03-11 10:33:24 -0300

    correção dos arquivos .java

---
+++ src/Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+    
+    private String cpf;
+    private String name;
+
+    public Customer(String cpf, String name) {
+        this.cpf = cpf;
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
