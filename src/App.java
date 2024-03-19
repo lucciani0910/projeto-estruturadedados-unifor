bca27ca1f5021fef49fd79fb5f6cd3eeade67bc0
nayla honorato <naylahonorato@hotmail.com>
2024-03-18 21:04:47 -0300

    upgrade no projeto estrutura de dados

--- src/App.java
+++ src/App.java
@@ -1,22 +1,6 @@
 public class App {
     public static void main(String[]args) {
-    // Exemplo de utilização
-        CustomerDatabase customerDatabase = new CustomerDatabase();
-    // Adicionar clientes ao banco de dados
-        Customer customer1 = new Customer("12345678901","João");
-        customerDatabase.addCustomer(customer1);
-        Customer customer2 = new Customer("98765432109","Maria");
-        customerDatabase.addCustomer(customer2);
-
-
-    // Buscar cliente pelo CPF
-        String cpf="12345678901";
-        Customer customer = customerDatabase.getCustomerByCpf(cpf);
-  
-        if (customer != null) {
-            System.out.println("Cliente encontrado:" + customer.getName());
-        } else {
-            System.out.println("Cliente não encontrado");
-        }
-    }
+        CaixaSupermercado caixa =new CaixaSupermercado();
+        caixa.executar();
+}
 }
---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    public Cliente(String nome,String sobrenome, String cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " "+ sobrenome + ", CPF: " + cpf;
+    }
+    
+}
